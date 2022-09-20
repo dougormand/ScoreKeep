0291dfcadff61c6eee1f0c97737508c188183cb4
dougormand <doug.ormand@gmail.com>
2022-09-20 10:56:28 -0700

    Added domain and webapi projects

---
+++ ScoreKeepService.Domain/Migrations/20220920170126_Stats.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ScoreKeepService.Domain.Migrations
+{
+    public partial class Stats : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Stats",
+                columns: table => new
+                {
+                    ID = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Stats", x => x.ID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Stats");
+        }
+    }
+}
---
+++ ScoreKeepService.Domain/ScoreKeepDbContext.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ScoreKeepService.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScoreKeepService.Domain
+{
+    public class ScoreKeepDbContext : DbContext
+    {
+
+        private readonly string _connectionString = @"Server=(localdb)\mssqllocaldb;Database=ScoreKeep";
+
+        //public ScoreKeepDbContext(DbContextOptions<ScoreKeepDbContext> options) : base(options) { }
+
+
+        public ScoreKeepDbContext()
+        {
+            //var contextOptions = new DbContextOptionsBuilder<ScoreKeepDbContext>()
+            //                        .UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=Test")
+            //                        .Options;
+            
+            //using var context = new ScoreKeepDbContext(contextOptions);
+        }
+
+        //protected override void onconfiguring(DbContextOptionsBuilder optionsbuilder)
+        //{
+        //    optionsbuilder.usesqlserver(_connectionString);
+        //    //base.onconfiguring(optionsbuilder);
+        //}
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(_connectionString);
+            //base.OnConfiguring(optionsBuilder);
+        }
+
+        public DbSet<Stat> Stats { get; set; }
+    }
+}
